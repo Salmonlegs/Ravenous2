ef7378dd7e93c8dba487f5dea2b3f3faba111b6f
salmonlegs <s.hardy11@live.com>
2018-11-11 12:54:09 +0800

    remove and add project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import BusinessList from './Components/BusinessList/BusinessList';
+import SearchBar from './Components/SearchBar/SearchBar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello Simon</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
